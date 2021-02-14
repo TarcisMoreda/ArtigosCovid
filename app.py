663690fb36c38e5b3e21740dafd7baba02ed32a3
Tárcis <tarcismoreda@gmail.com>
2021-02-14 18:21:42 -0300

    Adicionada a funcionalidade de pesquisa

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+from calendar import c
 import tkinter as tk
 import mysql.connector
 import datetime
 
 from tkinter.ttk import *
-from tkinter.constants import DISABLED, LEFT, NORMAL
+from tkinter.constants import DISABLED, END, NORMAL
 from tkinter import messagebox
 from tkcalendar import *
 
@@ -66,37 +67,66 @@ def selecionar_id():
 
     except:
         messagebox.showwarning('Erro', 'Selecione um item da lista.')
+        return 0
 
 def selecionar_ultimo_id():
     try:
         if len(dgv_artigos.get_children()) != 0:
             ultimo = dgv_artigos.get_children()[-1]
             return (dgv_artigos.item(ultimo).get('values')[0])
+        
+        else:
+            return 0
 
     except Exception as ex:
         messagebox.showwarning('Erro', ex)
 
-def escrever_textbox(titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, desafio):
+def escrever_textbox(id, titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, desafio):
     try:
         item_atual = dgv_artigos.focus()
         tudo = dgv_artigos.item(item_atual).get('values')
         data_formatada = datetime.datetime.strptime(str(tudo[2]), '%Y-%m-%d').strftime('%d/%m/%Y')
 
-        titulo.insert(0, tudo[1])
+        id.config(state=NORMAL)
+        id.delete(0, 'end')
+        id.insert(0, selecionar_id())
+        id.config(state=DISABLED)
+
+        titulo.insert(END, tudo[1])
         data.set_date(data_formatada)
-        base_dados.insert(0, tudo[3])
-        tecnica.insert(0, tudo[4])
-        acuracia.insert(0, tudo[5])
-        precisao.insert(0, tudo[6])
-        deficiencia.insert(0, tudo[7])
-        desafio.insert(0, tudo[8])
+        base_dados.insert(END, tudo[3])
+        tecnica.insert(END, tudo[4])
+        acuracia.insert(END, tudo[5])
+        precisao.insert(END, tudo[6])
+        deficiencia.insert(END, tudo[7])
+        desafio.insert(END, tudo[8])
     
     except Exception as ex:
             messagebox.showwarning('Erro', ex)
 
-    
+def bloquear_botoes():
+    btn_salvar_win.config(state=DISABLED)
+    btn_alterar_win.config(state=DISABLED)
+    btn_excluir.config(state=DISABLED)
+    btn_pesquisar.config(state=DISABLED)
 
-def salvar(titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, desafio, txt_id):
+def desbloquear_botoes(janela):
+    btn_salvar_win.config(state=NORMAL)
+    btn_alterar_win.config(state=NORMAL)
+    btn_excluir.config(state=NORMAL)
+    btn_pesquisar.config(state=NORMAL)
+    janela.destroy()
+
+def limpar_campos(titulo, base_dados, tecnica, acuracia, precisao, deficiencia, desafio):
+    titulo.delete("1.0", "end")
+    base_dados.delete("1.0", "end")
+    tecnica.delete("1.0", "end")
+    acuracia.delete(0, 'end')
+    precisao.delete(0, 'end')
+    deficiencia.delete("1.0", "end")
+    desafio.delete("1.0", "end")
+
+def salvar(titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, desafio, txt_id, txt_titulo, txt_base_dados, txt_tecnica, txt_acuracia, txt_precisao, txt_deficiencia, txt_desafio):
     if titulo == '' or tecnica == '' or acuracia == '' or precisao == '' or desafio == '' or base_dados == '' or  deficiencia == '':
         messagebox.showwarning('Erro', 'Preencha todos os campos.')
 
@@ -127,7 +157,12 @@ def salvar(titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, d
             txt_id.insert(0, selecionar_ultimo_id()+1)
             txt_id.config(state=DISABLED)
 
-def alterar(id, titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, desafio):
+            limpar_campos(txt_titulo, txt_base_dados, txt_tecnica, txt_acuracia, txt_precisao, txt_deficiencia, txt_desafio)
+
+def alterar(id, titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, desafio, janela):
+    if id == 0:
+        janela.destroy()
+    
     if titulo == '' or tecnica == '' or acuracia == '' or precisao == '' or desafio == '' or base_dados == '' or  deficiencia == '':
         messagebox.showwarning('Erro', 'Preencha todos os campos.')
         
@@ -152,6 +187,7 @@ def alterar(id, titulo, data, base_dados, tecnica, acuracia, precisao, deficienc
         finally:
             db.close()
             carregar_tabela()
+            desbloquear_botoes(janela)
 
 def excluir(id):
     try:
@@ -174,16 +210,30 @@ def excluir(id):
     finally:
         db.close()
 
-def bloquear_botoes():
-    btn_salvar_win.config(state=DISABLED)
-    btn_alterar_win.config(state=DISABLED)
-    btn_excluir.config(state=DISABLED)
+def pesquisar(termo, coluna):
+    resultado = []
+    carregar_tabela()
 
-def desbloquear_botoes(janela):
-    btn_salvar_win.config(state=NORMAL)
-    btn_alterar_win.config(state=NORMAL)
-    btn_excluir.config(state=NORMAL)
-    janela.destroy()
+    if coluna != 0 and termo != '':
+        for child in dgv_artigos.get_children():
+            if str(termo) in str(dgv_artigos.item(child)['values'][coluna-1]):
+                resultado.append(dgv_artigos.item(child)['values'])
+
+    else:
+        messagebox.showwarning('Erro', 'Selecione uma coluna para ser pesquisada ou preencha o termo de pesquisa.')
+
+    if resultado == []:
+        messagebox.showinfo('Oops', 'Não existe um registro com esse valor.')
+
+    else:
+        dgv_artigos.delete(*dgv_artigos.get_children())
+        
+        for row in resultado:
+            dgv_artigos.insert(parent='', index='end', values=(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]))
+
+def limpar_pesquisa():
+    txt_termo.delete(0, 'end')
+    carregar_tabela()
 
 def reg_window(tipo, dgv_artigos):
     bloquear_botoes()
@@ -195,100 +245,127 @@ def reg_window(tipo, dgv_artigos):
     if len(dgv_artigos.get_children()) == 0 and not tipo:
         messagebox.showwarning('Erro', 'Cadastre um item.')
         desbloquear_botoes(reg_window)
-        return
+
+    try:
+        dgv_artigos.item(dgv_artigos.focus()).get('values')[0]
+    
+    except:
+        messagebox.showwarning('Erro', 'Selecione um item.')
+        desbloquear_botoes(reg_window)
 
     lbl_id = tk.Label(reg_window, text='ID:')
     lbl_id.grid(column=0, row=0, padx=3, pady=3, sticky='W')
 
-    txt_id = tk.Entry(reg_window, width=5)
+    txt_id = tk.Entry(reg_window, width=5, state=DISABLED)
     txt_id.grid(column=0, row=1, padx=3, pady=3, sticky='NW')
 
-    lbl_titulo = tk.Label(reg_window, text='Título:')
-    lbl_titulo.grid(column=1, row=0, padx=3, pady=3, sticky='W')
-
-    txt_titulo = tk.Entry(reg_window, width=25, justify=LEFT)
-    txt_titulo.grid(column=1, row=1, padx=3, pady=3, sticky='NW')
-
     lbl_data = tk.Label(reg_window, text='Data:')
-    lbl_data.grid(column=2, row=0, padx=3, pady=3, sticky='W')
+    lbl_data.grid(column=1, row=0, padx=3, pady=3, sticky='W')
 
     dtp_data = DateEntry(reg_window, selectmode='day')
-    dtp_data.grid(column=2, row=1, padx=3, pady=3, sticky='NW')
-
-    lbl_base_dados = tk.Label(reg_window, text='Base de Dados:')
-    lbl_base_dados.grid(column=0, row=2, padx=3, pady=3, sticky='W')
-
-    txt_base_dados = tk.Entry(reg_window, width=25, justify=LEFT)
-    txt_base_dados.grid(column=0, row=3, padx=3, pady=3, sticky='NW')
-
-    lbl_tecnica = tk.Label(reg_window, text='Técnica Utilizada:')
-    lbl_tecnica.grid(column=1, row=2, padx=3, pady=3, sticky='W')
-
-    txt_tecnica = tk.Entry(reg_window, width=25, justify=LEFT)
-    txt_tecnica.grid(column=1, row=3, padx=3, pady=3, sticky='NW')
+    dtp_data.grid(column=1, row=1, padx=3, pady=3, sticky='NSEW')
 
     lbl_acuracia = tk.Label(reg_window, text='Acurácia:')
-    lbl_acuracia.grid(column=2, row=2, padx=3, pady=3, sticky='W')
+    lbl_acuracia.grid(column=2, row=0, padx=3, pady=3, sticky='W')
 
-    txt_acuracia = tk.Entry(reg_window, width=25, justify=LEFT)
-    txt_acuracia.grid(column=2, row=3, padx=3, pady=3, sticky='NW')
+    txt_acuracia = tk.Entry(reg_window, width=25)
+    txt_acuracia.grid(column=2, row=1, padx=3, pady=3, sticky='NSEW')
 
-    lbl_precisao = tk.Label(reg_window, text='Previsão:')
-    lbl_precisao.grid(column=0, row=4, padx=3, pady=3, sticky='W')
+    lbl_precisao = tk.Label(reg_window, text='Precisão:')
+    lbl_precisao.grid(column=3, row=0, padx=3, pady=3, sticky='W')
 
-    txt_precisao = tk.Entry(reg_window, width=25, justify=LEFT)
-    txt_precisao.grid(column=0, row=5, padx=3, pady=3, sticky='NW')
+    txt_precisao = tk.Entry(reg_window, width=25)
+    txt_precisao.grid(column=3, row=1, padx=3, pady=3, sticky='NSEW')
+
+    lbl_titulo = tk.Label(reg_window, text='Título:')
+    lbl_titulo.grid(column=0, row=2, padx=3, pady=3, sticky='W')
+
+    txt_titulo = tk.Text(reg_window, width=100, height=5)
+    txt_titulo.grid(column=0, row=3, padx=3, pady=3, sticky='NW', columnspan=4)
+
+    lbl_base_dados = tk.Label(reg_window, text='Base de Dados:')
+    lbl_base_dados.grid(column=0, row=4, padx=3, pady=3, sticky='W')
+
+    txt_base_dados = tk.Text(reg_window, width=100, height=5)
+    txt_base_dados.grid(column=0, row=5, padx=3, pady=3, sticky='NW', columnspan=4)
+
+    lbl_tecnica = tk.Label(reg_window, text='Técnica Utilizada:')
+    lbl_tecnica.grid(column=0, row=6, padx=3, pady=3, sticky='W')
+
+    txt_tecnica = tk.Text(reg_window, width=100, height=5)
+    txt_tecnica.grid(column=0, row=7, padx=3, pady=3, sticky='NW', columnspan=4)
 
     lbl_deficiencia = tk.Label(reg_window, text='Deficiência:')
-    lbl_deficiencia.grid(column=1, row=4, padx=3, pady=3, sticky='W')
+    lbl_deficiencia.grid(column=0, row=8, padx=3, pady=3, sticky='W')
 
-    txt_deficiencia = tk.Entry(reg_window, width=25, justify=LEFT)
-    txt_deficiencia.grid(column=1, row=5, padx=3, pady=3, sticky='NW')
+    txt_deficiencia = tk.Text(reg_window, width=100, height=5)
+    txt_deficiencia.grid(column=0, row=9, padx=3, pady=3, sticky='NW', columnspan=4)
 
     lbl_desafio = tk.Label(reg_window, text='Desafio:')
-    lbl_desafio.grid(column=2, row=4, padx=3, pady=3, sticky='W')
+    lbl_desafio.grid(column=0, row=10, padx=3, pady=3, sticky='W')
 
-    txt_desafio = tk.Entry(reg_window, width=25)
-    txt_desafio.grid(column=2, row=5, padx=3, pady=3, sticky='NW')
+    txt_desafio = tk.Text(reg_window, width=100, height=5)
+    txt_desafio.grid(column=0, row=11, padx=3, pady=3, sticky='NW', columnspan=4)
 
     if tipo:
-        btn_salvar = tk.Button(reg_window, text='Salvar', width=10, height=1, command=lambda: salvar(txt_titulo.get(), str(dtp_data.get_date()), txt_base_dados.get(), txt_tecnica.get(), txt_acuracia.get(), txt_precisao.get(), txt_deficiencia.get(), txt_desafio.get(), txt_id))
-        btn_salvar.grid(column=2, row=6, padx=3, pady=3, sticky='SE')
+        btn_salvar = tk.Button(reg_window, text='Salvar', width=10, height=2, command=lambda: salvar(txt_titulo.get("1.0",'end-1c'), str(dtp_data.get_date()), txt_base_dados.get("1.0",'end-1c'), txt_tecnica.get("1.0",'end-1c'), txt_acuracia.get(), txt_precisao.get(), txt_deficiencia.get("1.0",'end-1c'), txt_desafio.get("1.0",'end-1c'), 
+                                                                                                     txt_id, txt_titulo, txt_base_dados, txt_tecnica, txt_acuracia, txt_precisao, txt_deficiencia, txt_desafio))
+        btn_salvar.grid(column=0, row=13, padx=3, pady=3, sticky='NSEW', columnspan=4, rowspan=2)
         
+        txt_id.config(state=NORMAL)
         txt_id.delete(0, 'end')
         txt_id.insert(0, selecionar_ultimo_id()+1)
         txt_id.config(state=DISABLED)
         
     else:
-        btn_alterar = tk.Button(reg_window, text='Alterar', width=10, height=1, command=lambda: alterar(selecionar_id(), str(dtp_data.get_date()), dtp_data.get_date(), txt_base_dados.get(), txt_tecnica.get(), txt_acuracia.get(), txt_precisao.get(), txt_deficiencia.get(), txt_desafio.get()))
-        btn_alterar.grid(column=2, row=6, padx=3, pady=3, sticky='SE')
+        btn_alterar = tk.Button(reg_window, text='Alterar', width=10, height=2, command=lambda: alterar(selecionar_id(), txt_titulo.get("1.0",'end-1c'), str(dtp_data.get_date()), txt_base_dados.get("1.0",'end-1c'), txt_tecnica.get("1.0",'end-1c'), txt_acuracia.get(), txt_precisao.get(), txt_deficiencia.get("1.0",'end-1c'), txt_desafio.get("1.0",'end-1c'), reg_window))
+        btn_alterar.grid(column=0, row=13, padx=3, pady=3, sticky='NSEW', columnspan=4, rowspan=2)
 
+        txt_id.config(state=NORMAL)
         txt_id.delete(0, 'end')
         txt_id.insert(0, selecionar_id())
         txt_id.config(state=DISABLED)
 
-        escrever_textbox(txt_titulo, dtp_data, txt_base_dados, txt_tecnica, txt_acuracia, txt_precisao, txt_deficiencia, txt_desafio)
+        escrever_textbox(txt_id, txt_titulo, dtp_data, txt_base_dados, txt_tecnica, txt_acuracia, txt_precisao, txt_deficiencia, txt_desafio)
         
 window = tk.Tk()
 window.title('ARTIGOS')
 
+lbl_termo = tk.Label(window, text='Termo de Pesquisa:')
+lbl_termo.grid(column=0, row=0, padx=3, pady=3, sticky='W')
+
+txt_termo = tk.Entry(window, width=25)
+txt_termo.grid(column=0, row=1, padx=3, pady=3, sticky='NSEW')
+
+lbl_pesquisa = tk.Label(window, text='Coluna pesquisada:')
+lbl_pesquisa.grid(column=1, row=0, padx=3, pady=3, sticky='W')
+
+cbb_pesquisa = Combobox(window, width=25, values=['', 'ID', 'Título', 'Data', 'Base de Dados', 'Técnica', 'Acurácia', 'Precisão', 'Deficiência', 'Desafio'])
+cbb_pesquisa.grid(column=1, row=1, padx=2.5, pady=3, sticky='NSEW')
+
+btn_pesquisar = tk.Button(window, text='Pesquisar', width=10, height=2, command=lambda:pesquisar(txt_termo.get(), cbb_pesquisa.current()))
+btn_pesquisar.grid(column=2, row=0, padx=10, pady=3, sticky='NSW', rowspan=2)
+
+btn_limpar = tk.Button(window, text='Limpar\nPesquisa', width=10, height=2, command=limpar_pesquisa)
+btn_limpar.grid(column=2, row=0, padx=10, pady=3, sticky='NSE', rowspan=2)
+
 dgv_artigos = Treeview(window)
 criar_tabela()
-dgv_artigos.grid(column=0, row=0, padx=3, pady=3, sticky='N')
+dgv_artigos.grid(column=0, row=2, padx=3, pady=3, sticky='N', columnspan=3)
 
 scroll = tk.Scrollbar(window, orient='vertical', command=dgv_artigos.yview)
-scroll.grid(column=1, row=0, sticky='N')
+scroll.grid(column=2, row=2, sticky='NSE', pady=3)
+dgv_artigos.configure(yscrollcommand = scroll.set)
 
-dgv_artigos.configure(xscrollcommand = scroll.set)
 carregar_tabela()
 
 btn_salvar_win = tk.Button(window, text='Registrar\nNovo', width=10, height=2, command=lambda: reg_window(True, dgv_artigos))
-btn_salvar_win.grid(column=0, row=1, padx=3, pady=3, sticky='SW')
+btn_salvar_win.grid(column=0, row=3, padx=3, pady=3, sticky='NSEW')
 
 btn_alterar_win = tk.Button(window, text='Alterar\nSelecionado', width=10, height=2, command=lambda: reg_window(False, dgv_artigos))
-btn_alterar_win.grid(column=0, row=1, padx=3, pady=3, sticky='S')
+btn_alterar_win.grid(column=1, row=3, padx=3, pady=3, sticky='NSEW')
 
 btn_excluir = tk.Button(window, text='Excluir\nSelecionado', width=10, height=2, command=lambda: excluir(selecionar_id()))
-btn_excluir.grid(column=0, row=1, padx=3, pady=3, sticky='SE')
+btn_excluir.grid(column=2, row=3, padx=3, pady=3, sticky='NSEW')
 
 window.mainloop()
--- bd/testes_artigos.sql
+++ bd/testes_artigos.sql
@@ -28,12 +28,23 @@ CREATE TABLE `artigos` (
   `data` date NOT NULL,
   `base_dados` varchar(500) NOT NULL,
   `tecnica` varchar(500) NOT NULL,
+  `acuracia` varchar(500) NOT NULL,
   `precisao` varchar(500) NOT NULL,
   `deficiencia` varchar(500) NOT NULL,
   `desafio` varchar(500) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `artigos`
+--
+
+LOCK TABLES `artigos` WRITE;
+/*!40000 ALTER TABLE `artigos` DISABLE KEYS */;
+INSERT INTO `artigos` VALUES (2,'a','2021-02-14','a','a','a','a','a','a'),(3,'a','2021-02-14','a','a','a','a','a','a'),(4,'a','2021-02-14','a','a','a','a','a','a'),(5,'a','2021-02-14','a','a','a','a','a','a'),(6,'a','2021-02-14','a','a','a','a','a','a'),(7,'a	','2021-02-14','a	a','a','a','a','a','a'),(8,'a','2021-02-14','a','a','a','a','a','a'),(9,'a','2021-02-14','a','a','a','a','a','a'),(10,'a','2021-02-14','a','a','a','a','a','a'),(11,'a','2021-02-14','a','a','a','a','a','a');
+/*!40000 ALTER TABLE `artigos` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -44,4 +55,4 @@ CREATE TABLE `artigos` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-02-14 11:44:14
+-- Dump completed on 2021-02-14 14:49:43
