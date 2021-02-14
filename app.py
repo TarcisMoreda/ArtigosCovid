85849372ec12de35e492920a5a7bd601fb94c404
Tárcis <tarcismoreda@gmail.com>
2021-02-14 11:45:35 -0300

    Medidas de segurança para caso não exista registros no banco de dados e a adição do arquivo do banco de dados.

--- app.py
+++ app.py
@@ -10,48 +10,48 @@ from tkcalendar import *
 aberto = False
 
 def criar_tabela():
-    dgv_testes['columns'] = ('ID', 'Título', 'Data', 'Base de Dados', 'Técnica', 'Acurácia', 'Precisão', 'Deficiência', 'Desafio')
+    dgv_artigos['columns'] = ('ID', 'Título', 'Data', 'Base de Dados', 'Técnica', 'Acurácia', 'Precisão', 'Deficiência', 'Desafio')
 
-    dgv_testes.column('#0', width=0, minwidth=0, anchor='n')
-    dgv_testes.column('ID', width=25, minwidth=25, anchor='n')
-    dgv_testes.column('Título', width=100, minwidth=25, anchor='n')
-    dgv_testes.column('Data', width=100, minwidth=25, anchor='n')
-    dgv_testes.column('Base de Dados', width=100, minwidth=25, anchor='n')
-    dgv_testes.column('Técnica', width=100, minwidth=25, anchor='n')
-    dgv_testes.column('Acurácia', width=100, minwidth=25, anchor='n')
-    dgv_testes.column('Precisão', width=100, minwidth=25, anchor='n')
-    dgv_testes.column('Deficiência', width=100, minwidth=25, anchor='n')
-    dgv_testes.column('Desafio', width=100, minwidth=25, anchor='n')
+    dgv_artigos.column('#0', width=0, minwidth=0, anchor='n')
+    dgv_artigos.column('ID', width=25, minwidth=25, anchor='n')
+    dgv_artigos.column('Título', width=100, minwidth=25, anchor='n')
+    dgv_artigos.column('Data', width=100, minwidth=25, anchor='n')
+    dgv_artigos.column('Base de Dados', width=100, minwidth=25, anchor='n')
+    dgv_artigos.column('Técnica', width=100, minwidth=25, anchor='n')
+    dgv_artigos.column('Acurácia', width=100, minwidth=25, anchor='n')
+    dgv_artigos.column('Precisão', width=100, minwidth=25, anchor='n')
+    dgv_artigos.column('Deficiência', width=100, minwidth=25, anchor='n')
+    dgv_artigos.column('Desafio', width=100, minwidth=25, anchor='n')
 
-    dgv_testes.heading('#0', text='', anchor='n')
-    dgv_testes.heading('ID', text='ID', anchor='n')
-    dgv_testes.heading('Título', text='Título', anchor='n')
-    dgv_testes.heading('Data', text='Data', anchor='n')
-    dgv_testes.heading('Base de Dados', text='Base de Dados', anchor='n')
-    dgv_testes.heading('Técnica', text='Técnica', anchor='n')
-    dgv_testes.heading('Acurácia', text='Acurácia', anchor='n')
-    dgv_testes.heading('Precisão', text='Precisão', anchor='n')
-    dgv_testes.heading('Deficiência', text='Deficiência', anchor='n')
-    dgv_testes.heading('Desafio', text='Desafio', anchor='n')
+    dgv_artigos.heading('#0', text='', anchor='n')
+    dgv_artigos.heading('ID', text='ID', anchor='n')
+    dgv_artigos.heading('Título', text='Título', anchor='n')
+    dgv_artigos.heading('Data', text='Data', anchor='n')
+    dgv_artigos.heading('Base de Dados', text='Base de Dados', anchor='n')
+    dgv_artigos.heading('Técnica', text='Técnica', anchor='n')
+    dgv_artigos.heading('Acurácia', text='Acurácia', anchor='n')
+    dgv_artigos.heading('Precisão', text='Precisão', anchor='n')
+    dgv_artigos.heading('Deficiência', text='Deficiência', anchor='n')
+    dgv_artigos.heading('Desafio', text='Desafio', anchor='n')
 
 def carregar_tabela():
-    dgv_testes.delete(*dgv_testes.get_children())
+    dgv_artigos.delete(*dgv_artigos.get_children())
 
     try:
         db = mysql.connector.connect(
             host = 'localhost',
             user = 'root',
             passwd = 'Tamanda010203',
-            database = 'covid'
+            database = 'testes'
         )
         
         cursor = db.cursor()
-        cursor.execute('SELECT * FROM covid.testes;')
+        cursor.execute('SELECT * FROM testes.artigos;')
         resultados = cursor.fetchall()
         db.commit()
 
         for row in resultados:
-            dgv_testes.insert(parent='', index='end', values=(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]))
+            dgv_artigos.insert(parent='', index='end', values=(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]))
 
     except Exception as ex:
             messagebox.showwarning('Erro', ex)
@@ -61,23 +61,25 @@ def carregar_tabela():
 
 def selecionar_id():
     try:
-        item_atual = dgv_testes.focus()
-        return (dgv_testes.item(item_atual).get('values')[0])
+        item_atual = dgv_artigos.focus()
+        return (dgv_artigos.item(item_atual).get('values')[0])
+
     except:
         messagebox.showwarning('Erro', 'Selecione um item da lista.')
 
 def selecionar_ultimo_id():
     try:
-        ultimo = dgv_testes.get_children()[-1]
-        return (dgv_testes.item(ultimo).get('values')[0])
+        if len(dgv_artigos.get_children()) != 0:
+            ultimo = dgv_artigos.get_children()[-1]
+            return (dgv_artigos.item(ultimo).get('values')[0])
 
-    except:
-        messagebox.showwarning('Erro', 'Selecione um item da lista.')
+    except Exception as ex:
+        messagebox.showwarning('Erro', ex)
 
 def escrever_textbox(titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, desafio):
     try:
-        item_atual = dgv_testes.focus()
-        tudo = dgv_testes.item(item_atual).get('values')
+        item_atual = dgv_artigos.focus()
+        tudo = dgv_artigos.item(item_atual).get('values')
         data_formatada = datetime.datetime.strptime(str(tudo[2]), '%Y-%m-%d').strftime('%d/%m/%Y')
 
         titulo.insert(0, tudo[1])
@@ -104,11 +106,11 @@ def salvar(titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, d
             host = 'localhost',
             user = 'root',
             passwd = 'Tamanda010203',
-            database = 'covid'
+            database = 'testes'
             )
         
             cursor = db.cursor()
-            cursor.execute('INSERT INTO `covid`.`testes` (`titulo`, `data`, `base_dados`, `tecnica`, `acuracia`, `precisao`, `deficiencia`, `desafio`) VALUES ("%s", STR_TO_DATE("%s","%%Y-%%m-%%d"), "%s", "%s", "%s", "%s", "%s", "%s");' %(titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, desafio))
+            cursor.execute('INSERT INTO `testes`.`artigos` (`titulo`, `data`, `base_dados`, `tecnica`, `acuracia`, `precisao`, `deficiencia`, `desafio`) VALUES ("%s", STR_TO_DATE("%s","%%Y-%%m-%%d"), "%s", "%s", "%s", "%s", "%s", "%s");' %(titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, desafio))
             db.commit()
 
             messagebox.showinfo('Sucesso', 'Registrado com sucesso ao banco de dados.')
@@ -128,18 +130,18 @@ def salvar(titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, d
 def alterar(id, titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, desafio):
     if titulo == '' or tecnica == '' or acuracia == '' or precisao == '' or desafio == '' or base_dados == '' or  deficiencia == '':
         messagebox.showwarning('Erro', 'Preencha todos os campos.')
-
+        
     else:
         try:
             db = mysql.connector.connect(
             host = 'localhost',
             user = 'root',
             passwd = 'Tamanda010203',
-            database = 'covid'
+            database = 'testes'
             )
         
             cursor = db.cursor()
-            cursor.execute('UPDATE `covid`.`testes` SET `titulo` = "%s", `data` = STR_TO_DATE("%s","%%Y-%%m-%%d"), `base_dados` = "%s", `tecnica` = "%s", `acuracia` = "%s", `precisao` = "%s", `deficiencia` = "%s", `desafio` = "%s" WHERE `id` = %s' %(titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, desafio, id))
+            cursor.execute('UPDATE `testes`.`artigos` SET `titulo` = "%s", `data` = STR_TO_DATE("%s","%%Y-%%m-%%d"), `base_dados` = "%s", `tecnica` = "%s", `acuracia` = "%s", `precisao` = "%s", `deficiencia` = "%s", `desafio` = "%s" WHERE `id` = %s' %(titulo, data, base_dados, tecnica, acuracia, precisao, deficiencia, desafio, id))
             db.commit()
 
             messagebox.showinfo('Sucesso', 'Registro alterado com sucesso.')
@@ -157,11 +159,11 @@ def excluir(id):
         host = 'localhost',
         user = 'root',
         passwd = 'Tamanda010203',
-        database = 'covid'
+        database = 'testes'
         )
         
         cursor = db.cursor()
-        cursor.execute('DELETE FROM `covid`.`testes` WHERE id = %s;' %(id))
+        cursor.execute('DELETE FROM `testes`.`artigos` WHERE id = %s;' %(id))
         db.commit()
 
         messagebox.showinfo('Sucesso', 'Registro excluído com sucesso.')
@@ -173,22 +175,27 @@ def excluir(id):
         db.close()
 
 def bloquear_botoes():
-    btn_salvar.config(state=DISABLED)
-    btn_alterar.config(state=DISABLED)
+    btn_salvar_win.config(state=DISABLED)
+    btn_alterar_win.config(state=DISABLED)
     btn_excluir.config(state=DISABLED)
 
 def desbloquear_botoes(janela):
-    btn_salvar.config(state=NORMAL)
-    btn_alterar.config(state=NORMAL)
+    btn_salvar_win.config(state=NORMAL)
+    btn_alterar_win.config(state=NORMAL)
     btn_excluir.config(state=NORMAL)
     janela.destroy()
 
-def reg_window(tipo):
+def reg_window(tipo, dgv_artigos):
     bloquear_botoes()
 
     reg_window = tk.Toplevel()
-    reg_window.title('ADICIONAR TESTES')
+    reg_window.title('ADICIONAR ARTIGOS')
     reg_window.wm_protocol('WM_DELETE_WINDOW', lambda:desbloquear_botoes(reg_window))
+
+    if len(dgv_artigos.get_children()) == 0 and not tipo:
+        messagebox.showwarning('Erro', 'Cadastre um item.')
+        desbloquear_botoes(reg_window)
+        return
 
     lbl_id = tk.Label(reg_window, text='ID:')
     lbl_id.grid(column=0, row=0, padx=3, pady=3, sticky='W')
@@ -263,23 +270,23 @@ def reg_window(tipo):
         escrever_textbox(txt_titulo, dtp_data, txt_base_dados, txt_tecnica, txt_acuracia, txt_precisao, txt_deficiencia, txt_desafio)
         
 window = tk.Tk()
-window.title('TESTES COVID IA')
+window.title('ARTIGOS')
 
-dgv_testes = Treeview(window)
+dgv_artigos = Treeview(window)
 criar_tabela()
-dgv_testes.grid(column=0, row=0, padx=3, pady=3, sticky='N')
+dgv_artigos.grid(column=0, row=0, padx=3, pady=3, sticky='N')
 
-scroll = tk.Scrollbar(window, orient='vertical', command=dgv_testes.yview)
+scroll = tk.Scrollbar(window, orient='vertical', command=dgv_artigos.yview)
 scroll.grid(column=1, row=0, sticky='N')
 
-dgv_testes.configure(xscrollcommand = scroll.set)
+dgv_artigos.configure(xscrollcommand = scroll.set)
 carregar_tabela()
 
-btn_salvar = tk.Button(window, text='Registrar\nNovo', width=10, height=2, command=lambda: reg_window(True))
-btn_salvar.grid(column=0, row=1, padx=3, pady=3, sticky='SW')
+btn_salvar_win = tk.Button(window, text='Registrar\nNovo', width=10, height=2, command=lambda: reg_window(True, dgv_artigos))
+btn_salvar_win.grid(column=0, row=1, padx=3, pady=3, sticky='SW')
 
-btn_alterar = tk.Button(window, text='Alterar\nSelecionado', width=10, height=2, command=lambda: reg_window(False))
-btn_alterar.grid(column=0, row=1, padx=3, pady=3, sticky='S')
+btn_alterar_win = tk.Button(window, text='Alterar\nSelecionado', width=10, height=2, command=lambda: reg_window(False, dgv_artigos))
+btn_alterar_win.grid(column=0, row=1, padx=3, pady=3, sticky='S')
 
 btn_excluir = tk.Button(window, text='Excluir\nSelecionado', width=10, height=2, command=lambda: excluir(selecionar_id()))
 btn_excluir.grid(column=0, row=1, padx=3, pady=3, sticky='SE')
