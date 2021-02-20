34cfa5e3c764e6e500b8149695f62da83867531e
TarcisMoreda <66870414+TarcisMoreda@users.noreply.github.com>
2021-02-20 15:00:11 -0300

    Consertado os comandos

--- bd/testes_artigos.sql
+++ bd/testes_artigos.sql
@@ -1,27 +1,3 @@
--- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: testes
--- ------------------------------------------------------
--- Server version	8.0.23
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `artigos`
---
-
-DROP TABLE IF EXISTS `artigos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `artigos` (
   `id` int NOT NULL AUTO_INCREMENT,
   `titulo` varchar(500) NOT NULL,
@@ -33,26 +9,4 @@ CREATE TABLE `artigos` (
   `deficiencia` varchar(500) NOT NULL,
   `desafio` varchar(500) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `artigos`
---
-
-LOCK TABLES `artigos` WRITE;
-/*!40000 ALTER TABLE `artigos` DISABLE KEYS */;
-INSERT INTO `artigos` VALUES (2,'a','2021-02-14','a','a','a','a','a','a'),(3,'a','2021-02-14','a','a','a','a','a','a'),(4,'a','2021-02-14','a','a','a','a','a','a'),(5,'a','2021-02-14','a','a','a','a','a','a'),(6,'a','2021-02-14','a','a','a','a','a','a'),(7,'a	','2021-02-14','a	a','a','a','a','a','a'),(8,'a','2021-02-14','a','a','a','a','a','a'),(9,'a','2021-02-14','a','a','a','a','a','a'),(10,'a','2021-02-14','a','a','a','a','a','a'),(11,'a','2021-02-14','a','a','a','a','a','a');
-/*!40000 ALTER TABLE `artigos` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-02-14 14:49:43
+)
